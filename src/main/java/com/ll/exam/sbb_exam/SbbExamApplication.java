d8636f3e4b6d4304903348d761da70f37c1a9e53
SangWon7242 <gotkdqja@gmail.com>
2023-03-17 16:04:58 +0900

    세팅

---
+++ src/main/java/com/ll/exam/sbb_exam/SbbExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sbb_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbExamApplication.class, args);
+	}
+
+}

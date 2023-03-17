3bab8f8d9e5cf8910d8a7d682d781511ff38a888
SangWon7242 <gotkdqja@gmail.com>
2023-03-17 17:02:43 +0900

    @ResponseBody

--- src/main/java/com/ll/exam/sbb_exam/MainController.java
+++ src/main/java/com/ll/exam/sbb_exam/MainController.java
@@ -2,11 +2,19 @@ package com.ll.exam.sbb_exam;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
   @RequestMapping("/sbb")
-  public void index() {
-    System.out.println("123456");
+  // 아래 함수의 리턴값을 그대로 브라우저에 표시
+  // 아래 함수의 리턴값을 문자열화 해서 브라우저 응답을 바디에 담는다.
+  @ResponseBody
+  public String index() {
+    // 서버에서 실행
+    System.out.println("Hello"); // 콘솔 출력
+
+    // 먼 미래에서 브라우저에서 실행
+    return "안녕하세요."; // 브라우저에서 출력
   }
 }

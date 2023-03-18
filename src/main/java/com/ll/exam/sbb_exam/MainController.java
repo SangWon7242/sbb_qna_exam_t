28df5b343bb8c60385956ee80954fd3de594e1a8
SangWon7242 <gotkdqja@gmail.com>
2023-03-19 02:51:51 +0900

    생성자가 여러개라면, 인작없은생성자 선택

--- src/main/java/com/ll/exam/sbb_exam/MainController.java
+++ src/main/java/com/ll/exam/sbb_exam/MainController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -234,9 +235,16 @@ class Article {
 }
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 class Person {
   private int id;
   private int age;
   private String name;
+
+  public Person(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
 }

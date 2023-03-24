3ccf5184236922dc01e0a889c39fadbef0d44965
SangWon7242 <gotkdqja@gmail.com>
2023-03-25 03:35:00 +0900

    findById

--- src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
+++ src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbExamApplicationTests {
@@ -67,5 +69,14 @@ class SbbExamApplicationTests {
 		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	void testJpa6() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 }

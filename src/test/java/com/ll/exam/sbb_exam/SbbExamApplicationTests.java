2ad9177461b803f9afa3f666b1a4860912387579
SangWon7242 <gotkdqja@gmail.com>
2023-03-25 02:06:53 +0900

    테스트 케이스 추가

--- src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
+++ src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SbbExamApplicationTests {
 	@Autowired
@@ -21,12 +23,17 @@ class SbbExamApplicationTests {
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+		this.questionRepository.save(q1);
+
+		System.out.println(q1.getId());
 
 		Question q2 = new Question();
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		this.questionRepository.save(q2);
+
+		assertThat(q1.getId()).isGreaterThan(0);
+		assertThat(q2.getId()).isGreaterThan(q1.getId());
 	}
 }

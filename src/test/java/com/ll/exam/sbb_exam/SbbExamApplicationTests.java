755de19fc08854772f5e77a4ad57b8ef3a4f5a36
SangWon7242 <gotkdqja@gmail.com>
2023-03-25 23:01:35 +0900

    question truncate 테스트케이스 추가

--- src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
+++ src/test/java/com/ll/exam/sbb_exam/SbbExamApplicationTests.java
@@ -22,12 +22,12 @@ class SbbExamApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
+	void testJpa0() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+		questionRepository.save(q1);
 
 		System.out.println(q1.getId());
 
@@ -35,7 +35,26 @@ class SbbExamApplicationTests {
 		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		questionRepository.save(q2);
+
+		questionRepository.truncate();
+	}
+
+	@Test
+	void testJpa1() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+
+		System.out.println(q1.getId());
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);
 
 		assertThat(q1.getId()).isGreaterThan(0);
 		assertThat(q2.getId()).isGreaterThan(q1.getId());
@@ -66,14 +85,14 @@ class SbbExamApplicationTests {
 
 	@Test
 	void testJpa5() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		List<Question> qList = questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 	@Test
 	void testJpa6() {
-		Optional<Question> oq = this.questionRepository.findById(1);
+		Optional<Question> oq = questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 		q.setSubject("수정된 제목");
@@ -82,11 +101,11 @@ class SbbExamApplicationTests {
 
 	@Test
 	void testJpa7() {
-		assertEquals(2, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(1);
+		assertEquals(2, questionRepository.count());
+		Optional<Question> oq = questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1, this.questionRepository.count());
+		questionRepository.delete(q);
+		assertEquals(1, questionRepository.count());
 	}
 }

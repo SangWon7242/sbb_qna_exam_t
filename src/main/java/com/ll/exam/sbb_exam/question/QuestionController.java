8e7f12da3a67628ff2f95ced2dba24c1245b6710
SangWon7242 <gotkdqja@gmail.com>
2023-03-31 21:26:26 +0900

    컨트롤러 서비스 도입

--- src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
@@ -1,24 +1,29 @@
 package com.ll.exam.sbb_exam.question;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor // 생성자 주입
+// 컨트롤러는 Repository가 있는지 몰라야 한다.
+// 서비스는 브라우저라는 것이 이 세상에 존재하는지 몰라야한다.
+// 리포지터리는 서비스가 있는지 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야 한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+  // SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
   // @Autowired 필드 주입
-  private final QuestionRepository questionRepository;
+  private final QuestionService questionService;
 
   @GetMapping("/question/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일 뷰로 삼는다.
   public String list(Model model) {
-    List<Question> questionList = questionRepository.findAll();
+    List<Question> questionList = questionService.getList();
 
     // 미리애 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.

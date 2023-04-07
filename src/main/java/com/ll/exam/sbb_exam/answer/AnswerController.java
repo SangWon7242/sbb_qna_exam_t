01d4b21c063f037b6931ed1bdd904e8b6007a591
SangWon7242 <gotkdqja@gmail.com>
2023-04-08 01:52:25 +0900

    AnswerController 추가

---
+++ src/main/java/com/ll/exam/sbb_exam/answer/AnswerController.java
@@ -0,0 +1,30 @@
+package com.ll.exam.sbb_exam.answer;
+
+import com.ll.exam.sbb_exam.question.Question;
+import com.ll.exam.sbb_exam.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/answer")
+@Controller
+@RequiredArgsConstructor
+public class AnswerController {
+  private final QuestionService questionService;
+
+  @PostMapping("/create/{id}")
+  public String createAnswer(Model model, @PathVariable int id, String content) {
+    Question question = questionService.getQuestion(id);
+
+    // 답변 등록 시작
+    // 답변 등록 끝
+
+    return "redirect:/question/detail/%d".formatted(id);
+  }
+}

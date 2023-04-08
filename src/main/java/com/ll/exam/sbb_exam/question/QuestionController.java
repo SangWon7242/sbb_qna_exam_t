f875f001bc988e81db9af7c279a8b978fbb68ac0
SangWon7242 <gotkdqja@gmail.com>
2023-04-09 04:10:56 +0900

    폼 밸리데이션 적용하여 기존 로직 제거

--- src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
@@ -1,8 +1,10 @@
 package com.ll.exam.sbb_exam.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -42,26 +44,13 @@ public class QuestionController {
   }
 
   @GetMapping("/create")
-  public String questionCreate() {
+  public String questionCreate(QuestionForm questionForm) {
     return "question_form";
   }
 
   @PostMapping("/create")
-  public String questionCreate(Model model, QuestionForm questionForm) {
-    boolean hasError = false;
-
-    if(questionForm.getSubject() == null || questionForm.getSubject().trim().length() == 0) {
-      model.addAttribute("subjectErrorMsg", "제목 좀...");
-      hasError = true;
-    }
-
-    if(questionForm.getContent() == null || questionForm.getContent().trim().length() == 0) {
-      model.addAttribute("contentErrorMsg", "내용 좀...");
-      hasError = true;
-    }
-
-    if(hasError) {
-      model.addAttribute("questionForm", questionForm);
+  public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()) {
       return "question_form";
     }
 

5bc8376af6fbdf26148dcc1a7b2dcc4c9bf8cd2c
SangWon7242 <gotkdqja@gmail.com>
2023-04-22 02:53:31 +0900

    스프링 시큐리티로 로그아웃 처리

--- src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.extras.springsecurity5.util.SpringSecurityContextUtils;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 @RequestMapping("/question")
 @Controller
@@ -19,7 +23,7 @@ import javax.validation.Valid;
 // 서비스는 컨트롤러를 몰라야 한다.
 // DB는 리포지터리를 몰라야 한다.
 // SPRING DATA JPA는 MySQL을 몰라야 한다.
-  // SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
   // @Autowired 필드 주입
   private final QuestionService questionService;
@@ -27,11 +31,11 @@ public class QuestionController {
   @GetMapping("/list")
   // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일 뷰로 삼는다.
   public String list(Model model, @RequestParam(defaultValue = "0") int page) {
-    Page<Question> paging  = questionService.getList(page);
+    Page<Question> paging = questionService.getList(page);
 
     // 미리애 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
-    model.addAttribute("paging", paging );
+    model.addAttribute("paging", paging);
 
     return "question_list";
   }
@@ -51,7 +55,7 @@ public class QuestionController {
 
   @PostMapping("/create")
   public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
-    if(bindingResult.hasErrors()) {
+    if (bindingResult.hasErrors()) {
       return "question_form";
     }
 

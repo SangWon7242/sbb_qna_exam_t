522b80b0859ce9563d628f5dba5584f591fc3635
SangWon7242 <gotkdqja@gmail.com>
2023-04-23 02:42:55 +0900

    질문폼, 질문폼처리, 답변폼처리에 isAuthenticated 조건 걸기

--- src/main/java/com/ll/exam/sbb_exam/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb_exam/answer/AnswerController.java
@@ -5,6 +5,7 @@ import com.ll.exam.sbb_exam.question.QuestionService;
 import com.ll.exam.sbb_exam.user.SiteUser;
 import com.ll.exam.sbb_exam.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,6 +24,7 @@ public class AnswerController {
   private final AnswerService answerService;
   private final UserService userService;
 
+  @PreAuthorize("isAuthenticated()")
   @PostMapping("/create/{id}")
   public String createAnswer(Principal principal, Model model,@PathVariable long id,
                              @Valid AnswerForm answerForm, BindingResult bindingResult) {

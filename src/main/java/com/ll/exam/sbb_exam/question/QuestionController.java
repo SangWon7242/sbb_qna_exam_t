f1b92da12e1d6042c8c0702290cc05e293197a8e
SangWon7242 <gotkdqja@gmail.com>
2023-05-06 05:05:59 +0900

    th:with 로 조금 더 깔끔하게 페이지용 쿼리스트링을 공통으로 만듦

--- src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
@@ -44,8 +44,6 @@ public class QuestionController {
     // 미리애 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
     model.addAttribute("paging", paging);
-    model.addAttribute("kw", kw);
-    model.addAttribute("sortCode", sortCode);
 
     return "question_list";
   }

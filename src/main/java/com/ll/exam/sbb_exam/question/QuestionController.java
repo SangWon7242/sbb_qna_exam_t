fc9108dbefc97ff5676356273d06a5e9f542729a
SangWon7242 <gotkdqja@gmail.com>
2023-05-06 02:03:54 +0900

    페이지 이동을 해도 검색어가 풀리지 않도록

--- src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb_exam/question/QuestionController.java
@@ -44,6 +44,7 @@ public class QuestionController {
     // 미리애 실행된 question_list.html에서
     // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
     model.addAttribute("paging", paging);
+    model.addAttribute("kw", kw);
 
     return "question_list";
   }
